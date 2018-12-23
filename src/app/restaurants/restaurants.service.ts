ac5b8562bad9b0b8b419286acc9fe1445cd96d89
Patrick Amaral <patrick.dev.atom@gmail.com>
2018-12-23 11:27:39 -0200

    Rotas para avaliação e menu.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -16,5 +16,8 @@ export class RestaurantsService {
 
     restaurants(): Observable<Restaurant[]> {
       return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json()).catch(ErrorHandler.handleError);
-  }
+    }
+    restaurantById(id: string): Observable<Restaurant> {
+      return this.http.get(`${MEAT_API}/restaurants/${id}`).map(response => response.json()).catch(ErrorHandler.handleError);
+    }
 }
