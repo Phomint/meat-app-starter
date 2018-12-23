377210b70b60944fcbe007a193c7927c23b0a563
Patrick Amaral <patrick.dev.atom@gmail.com>
2018-12-23 13:12:22 -0200

    Implementando os itens de menu

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -8,6 +8,7 @@ import 'rxjs/add/operator/catch';
 import {Restaurant} from './restaurant/restaurant.model';
 import {MEAT_API} from '../app.api';
 import {ErrorHandler} from '../app.error-handler';
+import {MenuItem} from '../restaurant-detail/menu-item/menu-item.model';
 
 
 @Injectable()
@@ -22,5 +23,8 @@ export class RestaurantsService {
     }
     reviewsOfRestaurants(id: string): Observable<any> {
       return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`).map(response => response.json()).catch(ErrorHandler.handleError);
+    }
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+      return this.http.get(`${MEAT_API}/restaurants/${id}/menu`).map(response => response.json()).catch(ErrorHandler.handleError);
     }
 }
