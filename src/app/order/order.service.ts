0eb82dd70cfd24000620b71b09bb31e3b80f33d0
Patrick Amaral <patrick.dev.atom@gmail.com>
2019-01-04 16:33:29 -0200

    Finalizando a compra(HTTP POST)

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,10 +1,14 @@
 import {Injectable} from '@angular/core';
 import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model';
+import {Order} from './order.model';
+import {Observable} from 'rxjs';
+import {Headers, Http, RequestOptions} from '@angular/http';
+import {MEAT_API} from '../app.api';
 
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartService) {}
+  constructor(private cartService: ShoppingCartService, private http: Http) {}
   itemsValue(): number {
     return this.cartService.total();
   }
@@ -23,5 +27,16 @@ export class OrderService {
 
   remove(item: CartItem) {
     this.cartService.removeItem(item);
+  }
+
+  clear() {
+    this.cartService.clear();
+  }
+
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({headers: headers}))
+      .map(response => response.json());
   }
 }
