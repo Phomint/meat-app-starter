e6a0e69b8f63cc035f5e237a147ea98565c26a63
Patrick Amaral <patrick.dev.atom@gmail.com>
2019-01-15 15:11:04 -0200

    Middleware de autorização.

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,7 @@ import * as fs from 'fs';
 import * as https from 'https';
 
 import {handleAuthentication} from './auth';
+import {handleAuthorization} from './authz';
 
 const server: Express = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -20,7 +21,7 @@ server.use(jsonServer.bodyParser);
 server.use(router);
 
 server.post('/login', handleAuthentication);
-
+server.use('/orders', handleAuthorization)
 const options = {
   cert: fs.readFileSync('./backend/keys/cert.pem'),
   key: fs.readFileSync('./backend/keys/key.pem')
