31dfd201d28fedde80a532bd39806d55e7a09d43
Patrick Amaral <patrick.dev.atom@gmail.com>
2019-01-23 14:24:40 -0200

    Protegendo o modulo de compras

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,6 +11,7 @@ import {OrderService} from '../order/order.service';
 import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 import {NotificationService} from './messages/notification.service';
 import {LoginService} from '../security/login/login.service';
+import {LoggedinGuard} from '../security/loggedin.guard';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ export class SharedModule {
         RestaurantsService,
         OrderService,
         NotificationService,
-        LoginService
+        LoginService,
+        LoggedinGuard
       ]
     };
   }
